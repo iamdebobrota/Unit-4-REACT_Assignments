50d8c319bb9043ce094ecb060ab5356e9c27e1c6
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-04-27 17:27:23 +0530

    Add files via upload

---
+++ Sprint3_day4_Form/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+// import Counter from './Components/Counter';
+import Form from './Assignments-1/Form';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Counter/> */}
+<Form/>
+
+
+    </div>
+  );
+}
+
+export default App;
